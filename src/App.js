e59912738c6c69d9ea2b0cb96cf0ba317d25bb71
withsahil <sahil01.awasthi@gmail.com>
2020-08-30 15:35:30 +0530

    material-ui added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import User from './User'
+
 import { handleFetchUsers, handleAddUser, handleDeleteUser, handleUpdateUser } from './redux/actions/user.actions';
+import { Grid, IconButton, TextField } from '@material-ui/core';
+import { Add, Save } from '@material-ui/icons';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+
 
 class App extends Component {
   state = {
     name: '',
     job: ''
   }
+
+  useStyles = makeStyles((Theme) =>
+    createStyles({
+      root: {
+        '& > *': {
+          margin: Theme.spacing(1),
+        },
+      },
+    }),
+  );
 
   componentDidMount() {
     const { dispatch } = this.props;
@@ -32,17 +48,17 @@ class App extends Component {
   onUpdateUserHandler = () => {
     const { name, id } = this.state;
     const [first_name = '', last_name = ''] = name.split(' ')
-    
+
     this.props.dispatch(handleUpdateUser(first_name, last_name, id));
 
-    setTimeout(()=>{
+    setTimeout(() => {
       this.setState({
         first_name,
         last_name,
-        name:`${first_name} ${last_name}`
+        name: `${first_name} ${last_name}`
       })
-    },1500)
-    
+    }, 1500)
+
 
     this.setState({ name: '', job: '' })
   }
@@ -60,25 +76,44 @@ class App extends Component {
 
   render() {
     const { users } = this.props;
-    const { name, job, id } = this.state;
+    const { name, id } = this.state;
 
 
     return (
-      <div>
-        <div>
-          <input type="text" name="name" placeholder="name" value={name} onChange={this.handleChange} />
-          <input type="text" name="job" placeholder="job" value={job} onChange={this.handleChange} />
-          <button onClick={this.onAddUser}>Add User</button>
-          <button onClick={() => this.onUpdateUserHandler(name, id) } onChange={this.handleChange}>Update User</button>
-        </div>
-        {users.map(user => (
-          <User
-            {...user}
-            key={user.id}
-            onUpdateUser={this.onUpdateUser}
-            onDeleteUser={this.onDeleteUser} />
-        ))}
-      </div>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={5}>
+        <Grid container item xs={12} sm={6} md={6} lg={6}>
+          <Grid container item xs={12} p={5} direction="row" justify="center" alignItems="center">
+            <Grid container item xs={12} md={4} direction="row" justify="center" alignItems="center">
+              <TextField id="outlined-basic" variant="outlined" name="name" label="name" value={name} size="small" onChange={this.handleChange} />
+            </Grid>
+            <Grid container item xs={12} md={4} direction="row" justify="center" alignItems="center" >
+              <TextField id="outlined-basic" variant="outlined" name="job" label="job" size="small" onChange={this.handleChange} />
+            </Grid>
+            <Grid container item xs={6} md={1} direction="row" justify="center" alignItems="center">
+              <IconButton pt={5} onClick={this.onAddUser} >
+                <Add pt={5} />
+              </IconButton>
+            </Grid>
+            <Grid container item xs={6} md={1} direction="row" justify="center" alignItems="center">
+              <IconButton onClick={() => this.onUpdateUserHandler(name, id)} onChange={this.handleChange}>
+                <Save />
+              </IconButton>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12} spacing={4}>
+            {users.map(user => (
+              <Grid item xs={12} >
+                <User
+                  {...user}
+                  key={user.id}
+                  onUpdateUser={this.onUpdateUser}
+                  onDeleteUser={this.onDeleteUser} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
--- src/User.js
+++ src/User.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Grid, IconButton,List, ListItem, Divider, ListItemText, ListItemAvatar, Avatar, Typography } from '@material-ui/core';
+import { Delete, UpdateRounded } from '@material-ui/icons';
+
+
 
 const User = (props) => {
     const { first_name, last_name, id, onUpdateUser, onDeleteUser } = props;
 
     return (
-        <ul key={id}>
-            <li>
-                {first_name} {last_name}
-                <Button variant="contained" onClick={() => onDeleteUser(id)} >Delete</Button>
-                <Button variant="contained" color="primary" onClick={() => onUpdateUser(first_name, last_name, id)}>Update</Button>
-            </li>
-        </ul>
+        <Grid container xs={12} direction="row" justify="center" alignItems="center">
+            <List >
+                <Grid list>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar alt={first_name} src="/static/images/avatar/1.jpg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={first_name}
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        component="span"
+                                        variant="body2"
+                                        color="textPrimary">
+                                        My fullname is {first_name} {last_name}
+                                    </Typography>
+                                    {" — feel free to mail me at: "}
+                                </React.Fragment>} />
+
+
+                    </ListItem>
+                </Grid>
+                <Divider variant="inset" component="li" />
+                <Grid container xs={12} direction="row" justify="flex-end" alignItems="flex-end">
+                    <Grid container xs={1} direction="row" justify="center" alignItems="flex-end">
+                        <IconButton color="secondary" onClick={() => onDeleteUser(id)}>
+                            <Delete />
+                        </IconButton>
+                    </Grid>
+                    <Grid container xs={1} direction="row" justify="center" alignItems="flex-end">
+                        <IconButton color="primary" onClick={() => onUpdateUser(first_name, last_name, id)}>
+                            <UpdateRounded />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+
+            </List>
+        </Grid>
     );
 }
 
